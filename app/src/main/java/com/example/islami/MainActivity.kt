63f8966d5b6bc4f4780899ad76d5e73e54d8d9a4
Ahmed <ahmadadel@>
2024-01-17 21:12:37 +0200

    Make bottom nav bar actions and design the the four fragments

--- app/src/main/java/com/example/islami/MainActivity.kt
+++ app/src/main/java/com/example/islami/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.islami
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.islami.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadQuranFragment(savedInstanceState)
+
+        handleClicks()
     }
+
+    private fun handleClicks() {
+        binding.navBarBottom.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.quran -> {
+                    loadFragment(QuranFragment())
+                    true
+                }
+
+                R.id.hadeth -> {
+                    loadFragment(HadethFragment())
+                    true
+                }
+
+                R.id.radio -> {
+                    loadFragment(RadioFragment())
+                    true
+                }
+
+                R.id.sepha -> {
+                    loadFragment(SephaFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+    private fun loadQuranFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null)
+            loadFragment(QuranFragment())
+    }
+
 }
