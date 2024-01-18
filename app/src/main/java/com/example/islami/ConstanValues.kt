6896b109fb807aa66a0c36e61448232ec5f89c14
HP <ahmadadel@>
2024-01-18 17:09:37 +0200

    Design the night mode for the fragment layouts , code the sepha fragment, the set the actions and code the functions and create class for constant values

---
+++ app/src/main/java/com/example/islami/ConstanValues.kt
@@ -0,0 +1,10 @@
+package com.example.islami
+
+interface ConstantValues {
+    companion object {
+        const val firstZikr = "سبحان الله"
+        const val secondZikr = "الحمدلله"
+        const val thirdZikr = "الله اكبر"
+        const val fourthZikr = "الله لا اله هو له الملك وله الحمد "
+    }
+}
--- app/src/main/java/com/example/islami/SephaFragment.kt
+++ app/src/main/java/com/example/islami/SephaFragment.kt
@@ -5,9 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.islami.databinding.FragmentSephaBinding
 
 class SephaFragment : Fragment() {
 
+    private lateinit var binding: FragmentSephaBinding
+    private var texts = arrayListOf(
+        ConstantValues.firstZikr,
+        ConstantValues.secondZikr,
+        ConstantValues.thirdZikr,
+        ConstantValues.fourthZikr
+    )
+    private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,9 +25,34 @@ class SephaFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sepha, container, false)
+    ): View {
+        binding = FragmentSephaBinding.inflate(inflater, container, false)
+        onClick()
+        return binding.root
+    }
+
+    private fun changeZikrAndNumOfZikr() {
+        counter++
+        if (counter % 33 == 0) {
+            val index = (counter / 33) % texts.size
+            binding.sephaZikrTextView.text = texts[index]
+        }
+        val zikrCounter = counter.toString()
+        binding.sephaCounterTextView.text = zikrCounter
+    }
+
+    private fun rotateImage() {
+        binding.sephaBodyImage.rotation += 90f
+    }
+
+    private fun onClick() {
+        binding.sephaZikrTextView.text = texts[0]
+        binding.sephaBodyImage.setOnClickListener {
+            rotateImage()
+            changeZikrAndNumOfZikr()
+        }
     }
 
 }
+
+
